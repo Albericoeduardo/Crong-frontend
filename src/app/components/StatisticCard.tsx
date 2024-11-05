66fd50dcccfffcfae761b2eb1d3b288cc9b901de
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-11-05 09:16:53 -0300

    refactor: rename Card component to StatisticCard and add background gradient

--- src/app/components/StatisticCard.tsx
+++ src/app/components/StatisticCard.tsx
@@ -2,25 +2,27 @@
 import Image from 'next/image';
 import React from 'react';
 
-interface CardProps {
+interface StatisicCardProps {
   imageSrc: string;
   title: string;
   description: string;
 }
 
-const Card: React.FC<CardProps> = ({ imageSrc, title, description }) => {
+const StatisicCard: React.FC<StatisicCardProps> = ({ imageSrc, title, description }) => {
   return (
-    <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md w-64">
-      <Image
-        src={imageSrc}
-        alt={title}
-        width={140}
-        height={140}
-      />
-      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+    <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md w-64 gap-5">
+      <div className='flex flex-col items-center gap-5'>
+        <Image
+          src={imageSrc}
+          alt={title}
+          width={140}
+          height={140}
+        />
+        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      </div>
       <p className="text-center text-gray-600">{description}</p>
     </div>
   );
 };
 
-export default Card;
+export default StatisicCard;
