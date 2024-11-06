a0968defa06adbb7c98ec44c49c999e5c93a9507
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-11-06 15:43:59 -0300

    feat: add SectionHeader component to all sections for improved layout and context

--- src/app/layout/RecentsSection.tsx
+++ src/app/layout/RecentsSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import RecentsCard from '../components/RecentsCard'
+import SectionHeader from '../components/SectionHeader'
 
 const RecentsSection = () => {
     const recents = [
@@ -36,17 +37,22 @@ const RecentsSection = () => {
       ]
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12">
-      {recents.map((recent, index) => {
-        return (
-          <RecentsCard
-            imageSrc={recent.imagem}
-            title={recent.titulo}
-            description={recent.descricao}
-            key={index}
-          />
-        )
-      })}
+    <div>
+      <SectionHeader
+        title='ONGs Recém adicionadas'
+      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12">
+        {recents.map((recent, index) => {
+          return (
+            <RecentsCard
+              imageSrc={recent.imagem}
+              title={recent.titulo}
+              description={recent.descricao}
+              key={index}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
